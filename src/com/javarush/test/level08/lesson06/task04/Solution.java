443931ff10aae490007ada8bbec5596de3c7024d
SuprunO <oleksiisuprun@gmail.com>
2017-01-08 15:20:05 +0200

    Commit 08.01.2017 Level 8

---
+++ src/com/javarush/test/level08/lesson06/task04/Solution.java
@@ -0,0 +1,52 @@
+package com.javarush.test.level08.lesson06.task04;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+/* Измерить сколько времени занимает 10 тысяч вызовов get для каждого списка
+Измерить, сколько времени занимает 10 тысяч вызовов get для каждого списка.
+Метод getTimeMsOfGet  должен вернуть время его исполнения в миллисекундах.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        ArrayList<Object> arrayList =new ArrayList<Object>();
+        System.out.println(getTimeMsOfGet(fill(new ArrayList())));
+        LinkedList<Object> linkedList = new LinkedList<Object>();
+        System.out.println(getTimeMsOfGet(fill(new LinkedList())));
+    }
+
+    private static List fill(List list)
+    {
+        for(int i = 0; i < 11; i++)
+        {
+            list.add(new Object());
+        }
+        return list;
+    }
+
+    public static long  getTimeMsOfGet(List list)
+    {
+        //напишите тут ваш код
+        long currentTime = new Date().getTime();
+        get10000(list);
+        return new Date().getTime() - currentTime;
+
+        //напишите тут ваш ко
+    }
+
+    public static void get10000(List list)
+    {
+        if (list.isEmpty()) return;
+        int x = list.size() / 2;
+
+        for (int i = 0; i < 10000; i++)
+        {
+            list.get(x);
+        }
+    }
+}
